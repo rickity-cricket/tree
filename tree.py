eef6c54a291dad247c217bab325617f2d0604ccc
jake <jakemdoran@gmail.com>
2019-10-10 12:05:31 +0200

    Adding some style :)

--- tree.py
+++ tree.py
@@ -1,8 +1,20 @@
 import os
+from colorama import init, Fore
+init()
 
-current_dir = "."
-tree = ""
-MAX_DEPTH = 2
+def s(num):
+    r = ""
+    for i in range(0,num+1):
+        r += " "
+    return r
+
+current_dir = os.getcwd()
+print(u"\u2554" + s(len(current_dir)) + u" \u2557\n" 
+    + s(1) + current_dir 
+    + u"\n\u255a" + s(len(current_dir)) + u" \u255d")
+tree = Fore.LIGHTCYAN_EX + u"\u2550\u2550\u2550\u2566\n"
+
+MAX_DEPTH = 3
 EXCLUSIONS = ["node_modules", ".git", ".vscode", ".idea"]
 
 class File():
@@ -26,17 +38,23 @@ def expand_dir(dir):
 
 def tab(num):
     global tree
-    for i in range(0,3):
-        tree += "  "
+    for i in range(0,1):
+        tree += "   "
     if num > 0:
-        tree += u"\u01c1"
+        tree += Fore.LIGHTCYAN_EX + u"\u2563"
     for i in range(0,num):
-        tree += "  "
+        tree += "   "
 
 def add_file(file, level):
     global tree
+    colour = Fore.LIGHTGREEN_EX
+    if level is 0:
+        colour = Fore.LIGHTCYAN_EX
     tab(level)
-    tree += u"\u01c1`\u00d7 {}\n".format(file.name) # u00d7 => ×
+    if level is 0:
+        tree += colour + u"\u2560"
+    tree += colour + u"\u2023"
+    tree += colour + " {}\n".format(file.name) # u00d7 => ×
 
 def build_tree(start, base_depth):
     global tree
@@ -52,4 +70,5 @@ def build_tree(start, base_depth):
                 build_tree(os.path.join(f.path,f.name), base_depth)
 
 build_tree(os.getcwd(), len(os.getcwd().split("/")))
+tree += Fore.LIGHTCYAN_EX + u"\u2550\u2550\u2550\u2569"
 print(tree)
